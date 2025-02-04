6bb239e589712230dcd5f1206dcc917a254803be
Shashank-Raskas <shashankrk20@gmail.com>
2025-02-04 17:12:27 +0530

    added header userinputs and results components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import Header from "./components/Header"
+import UserInput from "./components/UserInput"
+import Results from "./components/Results"
+
 function App() {
+
   return (
-    <h1>React Investment Calculator</h1>
+    <div>
+      <Header />
+      <UserInput />
+      <Results />
+    </div>
   )
 }
 
---
+++ src/components/Results.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react"
+import { calculateInvestmentResults } from "../util/investment"
+
+export default function Results(){
+    return <section id='results'>
+        <h2>Results</h2>
+        <p>Results will be displayed here</p>
+    </section>
+}
