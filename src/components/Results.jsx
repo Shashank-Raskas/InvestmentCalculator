19821f113972780b5e63f1b1ddc48e1a0e6c2128
Shashank-Raskas <shashankrk20@gmail.com>
2025-02-04 17:26:19 +0530

    added state lifting from child components to app component so that it can be used by other child components

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react"
 import { calculateInvestmentResults } from "../util/investment"
 
-export default function Results(){
+export default function Results({input}){
+    console.log(input);
     return <section id='results'>
         <h2>Results</h2>
         <p>Results will be displayed here</p>
